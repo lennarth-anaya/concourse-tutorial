6ddb03dbc4981ab77db62affa6da639c6f11acc1
lennarth-anaya <lennarth.anaya@gmail.com>
2019-02-14 06:06:35 +0000

    Testing publishing-outputs'  double cloning (automatic default and manual one)

--- tutorials/basic/publishing-outputs/bump-timestamp-file.sh
+++ tutorials/basic/publishing-outputs/bump-timestamp-file.sh
@@ -5,8 +5,21 @@ set -x # print commands
 
 git clone resource-gist updated-gist
 
+echo "1-------------"
+
+cd resource-gist
+echo "2-------------"
+
 cd updated-gist
 echo $(date) > bumpme
+
+ls -l
+
+echo "resource-gist:"
+ls -l resource-gist
+
+echo "updated-gist:"
+ls -l updated-gist
 
 git config --global user.email "nobody@concourse-ci.org"
 git config --global user.name "Concourse"
